6b9e98e1f2e6245384f27aa3a75b9a3c0e13940f
Ian McNeil <ianmcneil2010@gmail.com>
2024-02-27 15:56:34 -0500

    Adds a few pages with working links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './family/contacts/contacts.component';
+import { FarmComponent } from './family/farm/farm.component';
+import { CalendarComponent } from './family/calendar/calendar.component';
+import { FoodComponent } from './family/food/food.component';
+import { AppComponent } from './app.component'; // Make sure the import is correct
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent }, // Updated here
+  { path: 'farm', component: FarmComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'food', component: FoodComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenericForTestingService } from './services/generic-for-testing.service';
@@ -10,13 +11,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule}  from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactsComponent } from './family/contacts/contacts.component';
+import { FoodComponent } from './family/food/food.component';
+import { FarmComponent } from './family/farm/farm.component';
+import { CalendarComponent } from './family/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    FoodComponent,
+    FarmComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule, 
+    RouterModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
